29a6363ca64d33c5ec8c1b2d1ac25f358b196873
Steven <Steven@DESKTOP-9UQB9C9>
2021-11-17 21:34:27 -0600

    Creación de los atributos de Jugador, get/setter y los constructores, tambien se creo la matriz vacia y el metodo para validar el ganador

---
+++ src/main/java/dao/Juego.java
@@ -0,0 +1,13 @@
+
+package dao;
+
+/**
+ * @author Steven
+ */
+public class Juego {
+    char matriz[][]= new char[3][3];
+    
+    private boolean validarGanador(){
+        return true;
+    }
+}
